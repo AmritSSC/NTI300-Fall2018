4cc1cdcdf69bdc483d942f4decb998d0f5eb0e21
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2018-09-27 20:34:59 -0700

    Update apache_install.sh

--- apache_install.sh
+++ apache_install.sh
@@ -1,7 +1,8 @@
-man su
 yum -y install httpd
 systemctl enable httpd
 systemctl start  httpd
 systemctl status  httpd
 yum -y install mod_ssl
 systemctl restart httpd
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
+echo "<html><body><h1> Hi there! </h1></body></html>" > /var/www/html/index.html
