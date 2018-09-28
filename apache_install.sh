f51edf54b548b765d6ac8b94145a3058317b651f
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2018-09-27 19:51:10 -0700

    first commit!
    Created to install script based on in-class work

---
+++ apache_install.sh
@@ -0,0 +1,10 @@
+man su
+yum install apache
+yum -y install apache httpd
+yum search apache
+systemctl enable http
+systemctl enable httpd
+systemctl start  httpd
+systemctl status  httpd
+yum -y install mod_ssl
+systemctl restart httpd
