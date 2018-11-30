e1672eb1a1623de019462487e4b01a5f960260c6
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2018-11-29 19:03:27 -0800

    Create instance-automation-django.py

---
+++ instance-automation-django.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+from oauth2client.client import GoogleCredentials
+from googleapiclient import discovery
+import pprint
+import json
+# import create_ldap
+# from create_ldap import create_instance
+
+credentials = GoogleCredentials.get_application_default()
+compute = discovery.build('compute', 'v1', credentials=credentials)
+
+project = "testproject1abc"
+zone = "us-east1-b"
+
+#type of machine requested and name for machine?
+#based on machine type, name can be derived
+
+name = 'test-instance1'
+
+# ########################################################
+
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items']
+    
+# ########################################################
+
+def create_instance(compute, project, zone, name):
+    startup_script = open('djangoInstallScript_python', 'r').read()
+    image_response = compute.images().getFromFamily(
+        project='centos-cloud', family='centos-7').execute()
+    
+    source_disk_image = image_response['selfLink']
+    machine_type = "zones/%s/machineTypes/f1-micro" % zone
+    
+    config = {
+        'name': name,
+        'machineType': machine_type,
+        
+        #specify the boot disk and image to use as source:
+        'disks': [
+           {
+              'boot': True,
+              'autoDelete': True,
+              'initializeParams': {
+                  'sourceImage': source_disk_image,
+               }
+           }
+         ],
+  
+    
+    # Specify a network interface with NAT to access the public
+    # internet.
+    'networkInterfaces': [{
+         'network': 'global/networks/default',
+         'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+      }],
+     
+     
+     # Allow the instance to access cloud storage and logging.
+    'serviceAccounts': [{
+         'email': 'default',
+         'scopes': [
+         'https://www.googleapis.com/auth/devstorage.read_write',
+         'https://www.googleapis.com/auth/logging.write'
+            ]
+      }],
+      
+      #Enable https/http for select instances:
+      "labels": {
+      "http-server": "",
+      "https-server": ""
+      },
+
+      "tags": {
+      "items": [
+      "http-server",
+      "https-server"
+      ]
+      },
+    
+    # Metadata is readable from the instance and allows you to
+    # pass configuration from deployment scripts to instances.
+    'metadata': {
+        'items': [{
+            # Startup script is automatically executed by the
+            # instance upon startup.
+            'key': 'startup-script',
+            'value': startup_script
+         }]
+       }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()     
+     
+# ####################################################################
+
+newinstance = create_instance(compute, project, zone, name)
+instances = list_instances(compute, project, zone)
+
+pprint.pprint(newinstance)
+pprint.pprint(instances)
